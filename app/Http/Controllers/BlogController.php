1ac27342b22314015a31169d5c265015a3ac7a1e
saad mohamed <itsme.rivo1@gmail.com>
2023-10-12 17:44:18 +0100

    blog controller resource backend api

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $blogs = Blog::all();
+        return response()->json([
+            "blogs" => $blogs
+        ],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(),[
+                'title' => 'required|string',
+                'body' => 'required|string',
+                'user_id' => 'required|integer',
+            ]);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => false ,
+                    "errors" => $validator->errors()
+                ],400);
+            }
+    
+            $validatedDataBlog = $validator->validate();
+    
+            $blog = Blog::create([
+                'title' => $validatedDataBlog['title'],
+                'body' => $validatedDataBlog['body'],
+                'user_id' => $validatedDataBlog['user_id'],
+            ]);
+    
+            return response()->json([
+                "message" => "Blog Created Seccessfully",
+                "blog" => $blog,
+            ],201) ;
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false ,
+                "errors" => $th->getMessage()
+            ],500);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        try {
+            $validator = Validator::make($request->all(),[
+                'title' => 'string',
+                'body' => 'string',
+            ]);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => false ,
+                    "errors" => $validator->errors()
+                ],400);
+            }
+    
+            $validatedDataBlog = $validator->validate();
+    
+            $blog->title = $validatedDataBlog['title'] ;
+            $blog->body = $validatedDataBlog['body'];
+            $blog->save();
+            
+            return response()->json([
+                "message" => "Blog Updated Seccessfully",
+                "blog" => $blog,
+            ],200) ;
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false ,
+                "errors" => $th->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        try {
+            if ($blog) {
+                $blog->delete();
+                return response()->json([
+                    "status" => false,
+                    "message" => "Blog deleted Successfuly"
+                ]);
+            } 
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false ,
+                "errors" => $th->getMessage()
+            ],500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -18,6 +19,8 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource("blogs",BlogController::class)->middleware('auth:sanctum');
 
 Route::post("/register",[UserController::class,'register']);
 Route::post("/login",[UserController::class,'login']);
